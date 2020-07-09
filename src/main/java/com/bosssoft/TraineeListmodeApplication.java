8f86c8a82137d6afddd3e47f66d325673a220529
wzh777 <15860920528@163.com>
2020-07-09 11:17:58 +0800

    新建spring-boot项目

---
+++ src/main/java/com/bosssoft/TraineeListmodeApplication.java
@@ -0,0 +1,13 @@
+package com.bosssoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeListmodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraineeListmodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bosssoft/TraineeListmodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosssoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeListmodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
